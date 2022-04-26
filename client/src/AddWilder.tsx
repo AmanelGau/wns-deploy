7575656e8b6d11ae2801a90d7c503c35f643892c
Amanel Gauriat <a.gauriat@monkey-tie.com>
2022-04-26 14:56:35 +0200

    language to french

--- client/src/AddWilder.tsx
+++ client/src/AddWilder.tsx
@@ -1,53 +1,51 @@
 import React, { useState } from "react";
-import { gql, useMutation } from '@apollo/client';
+import { gql, useMutation } from "@apollo/client";
 import { Button, Form, Input, Label } from "./styles/form-elements";
 import { WilderData } from "./App";
 
-
 const CREATE_WILDER = gql`
-mutation AddWilderMutation($addWilderWilder: WilderInput!) {
-  addWilder(wilder: $addWilderWilder) {
-    name
+  mutation AddWilderMutation($addWilderWilder: WilderInput!) {
+    addWilder(wilder: $addWilderWilder) {
+      name
+    }
   }
-}`;
-
+`;
 
 function AddWilder() {
   const [name, setName] = useState("");
   const [city, setCity] = useState("");
 
-  const [addWilder, { data, error }] = useMutation<{addWilder: WilderData}>(CREATE_WILDER);
+  const [addWilder, { data, error }] =
+    useMutation<{ addWilder: WilderData }>(CREATE_WILDER);
   if (error) return <p>Error : {error.message}</p>;
   return (
     <Form
       onSubmit={(e) => {
         e.preventDefault();
-        addWilder(
-          {
-            variables: {
-              addWilderWilder: {
-                name,
-                city
-              }
-            }
-          }
-        )
+        addWilder({
+          variables: {
+            addWilderWilder: {
+              name,
+              city,
+            },
+          },
+        });
       }}
     >
-      {data && <p>wilder { data.addWilder.name } a été ajouté.e</p>}
-      <Label htmlFor="name-input">Name :</Label>
+      {data && <p>wilder {data.addWilder.name} a été ajouté.e</p>}
+      <Label htmlFor="name-input">Nom :</Label>
       <Input
         id="name-input"
         type="text"
-        placeholder="Type the name"
+        placeholder="Entrer le nom"
         value={name}
         onChange={(e) => setName(e.target.value)}
       />
-      <Label htmlFor="city-input">City :</Label>
+      <Label htmlFor="city-input">Ville :</Label>
       <Input
         id="city-input"
         type="text"
-        placeholder="Type the city"
+        placeholder="Entrer la ville"
         value={city}
         onChange={(e) => setCity(e.target.value)}
       />
